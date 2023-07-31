a41f794ca75f7ab3f04cad0e0570c35c29bbe634
Engine <rg.dartigue@gmail.com>
2023-07-31 19:28:04 -0400

    added global constants to server.js file, linked to express, path, and index.js. noTe will init an instance of express

---
+++ server.js
@@ -0,0 +1,7 @@
+///
+const express = require('express');
+const path = require('path');
+const api = require('./public/assets/js/index')
+
+////// Init instance of express ==================/
+const noTe = express();
