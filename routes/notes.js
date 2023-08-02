4d12e6369da23e95a67251e4f1f9ff1bb6bf077b
Engine <rg.dartigue@gmail.com>
2023-08-01 22:54:07 -0400

    MAJOR FIX: app.get at line 18 and 22 missing curly brackets(fixed)

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,8 @@ const notes = require('express').Router();
 const store = require('../db/store');
 const path = require('path');
 
-app.get('/notes' , (req, res) =>
+notes.get('/notes' , (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+});
 
+module.exports = notes;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./public/assets/js/index.js');
 
-////// Init instance of express ==================/
+////// Init instance of express ====================================/
 const app = express();
 
 const PORT = 3001;
@@ -14,10 +14,18 @@ app.use('/api', api);
 app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
+// GET route Startpage=========================================================/
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+});
+// GET route Notespage=========================================================/
+app.get('/notes' , (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
